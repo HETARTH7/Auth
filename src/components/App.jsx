765bd760a9ea0e9250538208b0260b5e52221de2
unknown <hetarth002@gmail.com>
2022-10-16 21:30:59 +0530

    create login page

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Preferences from "./Preferences";
+
+function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
+  return (
+    <div>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
