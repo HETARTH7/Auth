1832d7aae92986dc09f2ae97f456e352e50c1b92
unknown <hetarth002@gmail.com>
2022-11-26 16:27:36 +0530

    manual auth

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 
 const Login = () => {
   const [user, setUser] = useState("");
-  const handleChange = (e) => {
+  const [password, setPassword] = useState("");
+  const handleUser = (e) => {
     setUser(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
   };
   const login = (e) => {
     e.preventDefault();
-    sessionStorage.setItem("user", user);
-    window.location = "/dashboard";
+    if (user === "hetarth") {
+      if (password === "hello") {
+        sessionStorage.setItem("user", user);
+        window.location = "/dashboard";
+      } else{
+        console.log('Incorrect details');
+      }
+    } else{
+      console.log('Incorrect details');
+    }
   };
   return (
     <div>
       <form onSubmit={login}>
-        <input onChange={handleChange}></input>
+        <input onChange={handleUser}></input>
+        <input type="password" onChange={handlePassword}></input>
         <input type="submit"></input>
       </form>
     </div>
