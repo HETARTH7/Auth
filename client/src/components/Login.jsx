0e8bf1f5bafa93be1b97cff2591889e6efc8cf89
unknown <hetarth002@gmail.com>
2022-11-30 00:33:26 +0530

    user auth

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 const Login = () => {
-  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = useState("Wrong");
   const [mssg, setMssg] = useState("");
   const errormssg = "Invalid Details. Please try again";
   const handleUser = (e) => {
-    setUser(e.target.value);
+    setUsername(e.target.value);
   };
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
   const login = (e) => {
     e.preventDefault();
+    const user = { username, password };
+    console.log(user);
+    axios
+      .post("http://localhost:5000/auth", user)
+      .then((res) => setAuth(res.data.message))
+      .catch((err) => console.log(err));
     try {
-      if (!auth) throw errormssg;
+      if (auth === "Wrong") throw errormssg;
+      else {
+        sessionStorage.setItem("user", username);
+        window.location = "/dashboard";
+      }
     } catch (err) {
       setMssg(err);
     }
