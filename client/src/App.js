2646b325fb7769b2199ef0b442a0a3411b30c8ab
unknown <hetarth002@gmail.com>
2023-07-14 22:19:36 +0530

    frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Home />
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div><h1>Auth</h1></div>
-  )
-}
+    <div className="text-center">
+      <h1>Auth</h1>
+      <Link to={"/login"} className="btn btn-success m-5">
+        Login
+      </Link>
+      <Link to={"/register"} className="btn btn-success m-5">
+        Register
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
