208ad31d31ce88ab772cabbd9d02fa7a2736b9d4
unknown <hetarth002@gmail.com>
2023-10-24 19:56:48 +0530

    register backend

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+const login = async (req, res) => {};
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { login, register };
