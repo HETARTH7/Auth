312ee85e7c7aeae0c9c62f70994c4c901a45cd30
unknown <hetarth002@gmail.com>
2023-11-10 21:25:50 +0530

    add login and signup forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
