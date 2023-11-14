305f471c528d392163f12b05721e837b767b2ed6
unknown <hetarth002@gmail.com>
2023-11-14 09:06:37 +0530

    set inital auth status

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useLogout } from "../hooks/useLogout";
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Home = () => {
   const { logout } = useLogout();
   const navigate = useNavigate();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -13,6 +15,7 @@ const Home = () => {
   return (
     <div>
       <button onClick={handleClick}>Log out</button>
+      <span>{user.email}</span>
     </div>
   );
 };
